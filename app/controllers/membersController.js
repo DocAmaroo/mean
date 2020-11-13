547e25bced39a54d51da0826dc88237d44474925
Kevin <a2509kgm@gmail.com>
2020-11-13 14:58:50 +0100

    patch

--- app/controllers/membersController.js
+++ app/controllers/membersController.js
@@ -1,11 +1,11 @@
-const membersModels = require('../models/membersModels');
+const Members = require('../models/membersModels');
 
 /**
  * Handle member authentification
  */
 exports.checkMember = function (req, res) {
     try {
-        membersModels.find(req.body)
+        Members.find(req.body)
             .then(function(members) {
                 if (members != undefined && members.length == 1) {
                     res.send({
@@ -25,5 +25,58 @@ exports.checkMember = function (req, res) {
             resultat: 0,
             message: e
         });
+    }
+};
+
+exports.createMember = function(req, res) {
+    let tmp_mail = req.query.mail;
+    try {
+        Members.findOne({mail:tmp_mail}, function (err, product) {
+            if(err) return res.status(500).send(err);
+            if(product != null) 
+                return res.status(404).json({
+                    ok:false,
+                    code:'404',
+                    message:"This mail is already use."
+                });
+            else{
+                let tmp_name = req.query.name;
+                let tmp_firstname = req.query.firstname;
+                let tmp_password = req.query.password;
+        
+                var newMember = new Members();
+                newMember.name = tmp_name;
+                newMember.firstname = tmp_firstname;
+                newMember.mail = tmp_mail;
+                newMember.password = tmp_password;
+        
+                newMember.save(function (err) {
+                    if(err) return res.status(500).send(err);
+                    else console.log("[+]Member registered\n");
+                    return res.status(201);
+                })
+            }
+        });
+    } catch (e) {
+        console.log("[-]Error on /member/register: query didn't work properly\n" + e);
+        res.json({
+            resultat: 0,
+            message: e
+    });
+}
+};
+
+/**
+ * Return all members
+ */
+exports.getMembers = function(req, res) {
+    try {
+        Members.find()
+        .then(function(m) {
+            res.json(m);
+        });
+    } catch (e) {
+        console.log("[-]Error on /members: query didn't work properly\n" + e);
+        res.json([]);
     }
 };
--- app/routes/members/index.js
+++ app/routes/members/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const membersController = require('../../controllers/membersController');
 
-
 router.route('/member/auth')
     .post(membersController.checkMember);
+
+router.route('/member/register')
+    .post(membersController.createMember);
+
+router.route('/members')
+    .get(membersController.getMembers);
+
 
 module.exports = router;
