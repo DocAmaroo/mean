630ba1d8e865aab245caa23d62b20e238ec18eea
Gracia-Moulis Kevin <kevin.gracia-moulis@etu.umontpellier.fr>
2020-11-13 12:01:13 +0100

    triggered

--- assets/static/scripts/creationBase.sh
+++
@@ -1,4 +0,0 @@
-mongoimport --db SUPERVENTES --collection members --file ../data/members.json --jsonArray --drop
-mongoimport --db SUPERVENTES --collection products --file ../data/products.json --jsonArray --drop
-mongoimport --db SUPERVENTES --collection carts --file ../data/carts.json --jsonArray --drop
-# mongoimport --db SUPERVENTES --collection marques --file ../data/marques.json --jsonArray --drop
