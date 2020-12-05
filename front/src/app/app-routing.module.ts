03cb4f8c76019269b9155c762b809677f0db3663
Kevin <a2509kgm@gmail.com>
2020-12-05 04:40:39 +0100

    p

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: '', component: ProductsComponent}, //home serait mieux
   {path: 'products', component: ProductsComponent},
-  {path: 'categories', component: CategoriesComponent}
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'connexion', component: ConnexionComponent},
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    ConnexionComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
